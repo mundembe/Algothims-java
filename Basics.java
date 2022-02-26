6b14406ced0c7541c62e8e33d8fa9d65c0de07d9
mundembe <61733352+mundembe@users.noreply.github.com>
2022-02-27 00:41:54 +0200

    Create Basics.java
    Basics of working with Arrays

---
+++ Basics.java
@@ -0,0 +1,79 @@
+public class Basics {
+
+    private int[] theArray = new int[50];
+    private int arraySize = 10;
+
+    public void generateValues() {
+        for(int i = 0; i < arraySize; i++){
+            theArray[i] = (int) (Math.random()*10 + 10);
+        }
+    }
+    public void printArray(){
+        System.out.println("----------");
+        for (int i = 0; i < arraySize; i++) {
+            System.out.print(" | " + i + " | ");
+            System.out.println(theArray[i]);
+            System.out.println("----------");
+        }
+    }
+    public int getValueAtIndex(int index){
+        if(index < arraySize) return theArray[index];
+        return 0;
+    }
+    public boolean doesArrayContainThisValue(int value){
+        boolean valueFound = false;
+
+        for (int i = 0; i < arraySize; i++) {
+            if (theArray[i] == value)
+                valueFound = true;
+        }
+
+        return valueFound;
+    }
+    public void deleteIndex(int index){
+        for (int i = index; i < (arraySize - 1); i++) {
+            theArray[i] = theArray[i+1];
+        }
+        arraySize--;
+    }
+    public void insertValue(int value){
+        if(arraySize < 50){
+            theArray[arraySize] = value;
+            arraySize++;
+        }
+    }
+    public String linearSearchForValue(int value){
+        boolean isValueFound = false;
+        String valuesFoundAt = "";
+
+        System.out.println("The value was found in the following indices");
+        for (int i = 0; i < arraySize; i++) {
+            if (theArray[i] == value){
+                valuesFoundAt += (i + " ");
+                isValueFound = true;
+            }
+        }
+        if (isValueFound == false)
+            valuesFoundAt = "Value not in array";
+
+        System.out.print(valuesFoundAt);
+
+        return valuesFoundAt;
+    }
+
+    public static void main(String[] args) {
+
+        Basics newArray = new Basics();
+        newArray.generateValues();
+        newArray.printArray();
+        System.out.println(newArray.getValueAtIndex(6));
+        System.out.println(newArray.doesArrayContainThisValue(13));
+        System.out.println("deleting value:");
+        newArray.deleteIndex(5);
+        newArray.printArray();
+        System.out.println("inserting value:");
+        newArray.insertValue(11);
+        newArray.printArray();
+        newArray.linearSearchForValue(11);
+    }
+}
